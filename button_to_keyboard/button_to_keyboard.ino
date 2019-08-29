d402cc445178d781be8d23957d0c691e69dd9bd4
Cole Purbaugh <colepurbaugh@gmail.com>
2019-08-29 11:51:19 -0700

    binary export update
    enabled led interactivity
    binary for flashing exported

--- button_to_keyboard/button_to_keyboard.ino
+++ button_to_keyboard/button_to_keyboard.ino
@@ -1,21 +1,32 @@
 #include "Keyboard.h"
 
+//physical button tied to 3.3v
 const int buttonPin = 2;
-const int triggerPin = 3;
-int buttonState = 0;
+const int ledPin = 13;
+
+int toggle = 1;         //one press of button is one letter, does not repeat
+int buttonState = 0;    //whether the button is pressed or not
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLDOWN);
-  pinMode(triggerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   Keyboard.begin();
 }
 
+/*
+ * Press the button, get a typed letter
+ * LED turns on when the button is pressed
+ */
 void loop() {
   buttonState = digitalRead(buttonPin);
-  if (buttonState){
-    Keyboard.print("j");
-    digitalWrite(triggerPin, HIGH);
-  } else {
-    digitalWrite(triggerPin, LOW);
+  if (buttonState && toggle){
+    Keyboard.print("f");
+    digitalWrite(ledPin, HIGH);
+    toggle = 0;
+    delay(100);
+  } else if (!buttonState && !toggle) {
+    digitalWrite(ledPin, LOW);
+    toggle = 1;
+    delay(100);
   }
 }
